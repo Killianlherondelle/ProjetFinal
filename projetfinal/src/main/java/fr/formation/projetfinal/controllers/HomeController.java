94263d2e475437a278cee14d03c716dc06398077
younes <younes.sahnoun17@gmail.com>
2018-09-04 14:31:11 +0200

    HomeController

---
+++ projetfinal/src/main/java/fr/formation/projetfinal/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package fr.formation.projetfinal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class HomeController {
+	
+	@GetMapping("/login")
+	public String welcome() {
+		return "login";
+	   }
+
+}
