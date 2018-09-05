de5fd73b3635b1228d84d06d8a3b2437dc451a67
Freddy <freddylas23@gmail.com>
2018-09-05 10:40:10 +0200

    Correction bug pour se logger en tant que User (security config).

--- projetfinal/src/main/java/fr/formation/projetfinal/WebConfig.java
+++ projetfinal/src/main/java/fr/formation/projetfinal/WebConfig.java
@@ -10,79 +10,77 @@ import org.springframework.web.servlet.i18n.*;
 import org.springframework.web.servlet.view.*;
 
 @Configuration
-@Import(value = { AppConfig.class })
+@Import(value = { AppConfig.class, SecurityConfig.class })
 @EnableWebMvc
 @ComponentScan("fr.formation.projetfinal.controllers")
 public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * Configuration des ressources statiques afin d'éviter les filtres inutiles
-     * sur les requêtes.
-     * <p>
-     * Classiquement tous les fichiers JS, CSS et images.
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	registry.addResourceHandler("/static/**")
-		.addResourceLocations("/static/");
-    }
+	/**
+	 * Configuration des ressources statiques afin d'ï¿½viter les filtres inutiles sur
+	 * les requï¿½tes.
+	 * <p>
+	 * Classiquement tous les fichiers JS, CSS et images.
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
 
-    /**
-     * Configuration des JSP, chemin et extension.
-     */
-    @Bean
-    public ViewResolver viewResolver() {
-	InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-	viewResolver.setViewClass(JstlView.class);
-	viewResolver.setPrefix("/WEB-INF/jspf/");
-	viewResolver.setSuffix(".jsp");
-	return viewResolver;
-    }
+	/**
+	 * Configuration des JSP, chemin et extension.
+	 */
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jspf/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
 
-    /**
-     * Configuration des fichiers properties contenant les messages
-     * externalisés.
-     */
-    @Bean
-    public ReloadableResourceBundleMessageSource messageSource() {
-	ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
-	source.setBasename("classpath:messages");
-	return source;
-    }
+	/**
+	 * Configuration des fichiers properties contenant les messages externalisï¿½s.
+	 */
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+		source.setBasename("classpath:messages");
+		return source;
+	}
 
-    /**
-     * Configuration de du paramètre de langue pour l'application.
-     */
-    @Bean
-    public CookieLocaleResolver localeResolver() {
-	CookieLocaleResolver resolver = new CookieLocaleResolver();
-	resolver.setCookieName(AppLanguage.CODE);
-	resolver.setDefaultLocale(AppLanguage.getDefault().getLocale());
-	return resolver;
-    }
+	/**
+	 * Configuration de du paramï¿½tre de langue pour l'application.
+	 */
+	@Bean
+	public CookieLocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setCookieName(AppLanguage.CODE);
+		resolver.setDefaultLocale(AppLanguage.getDefault().getLocale());
+		return resolver;
+	}
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-	LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-	interceptor.setParamName(AppLanguage.CODE);
-	registry.addInterceptor(interceptor);
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName(AppLanguage.CODE);
+		registry.addInterceptor(interceptor);
+	}
 
-    /**
-     * Configuration des fichiers properties contenant les messages d'erreur de
-     * validation.
-     */
-    @Bean
-    public ReloadableResourceBundleMessageSource validationMessageSource() {
-	ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	messageSource.setBasename("classpath:errors");
-	return messageSource;
-    }
+	/**
+	 * Configuration des fichiers properties contenant les messages d'erreur de
+	 * validation.
+	 */
+	@Bean
+	public ReloadableResourceBundleMessageSource validationMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:errors");
+		return messageSource;
+	}
 
-    @Override
-    public Validator getValidator() {
-	LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
-	validator.setValidationMessageSource(validationMessageSource());
-	return validator;
-    }
+	@Override
+	public Validator getValidator() {
+		LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(validationMessageSource());
+		return validator;
+	}
 }
