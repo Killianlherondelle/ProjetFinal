cf907e3e0610da54b272790448f0acb7da692b9d
Killian L'HERONDELLE <killianlherondelle@gmail.com>
2018-09-06 14:03:58 +0200

    update Finances for annotations

--- projetfinal/src/main/java/fr/formation/projetfinal/entities/Finances.java
+++ projetfinal/src/main/java/fr/formation/projetfinal/entities/Finances.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
@@ -46,8 +49,8 @@ public class Finances  implements Serializable {
     @Column(length = 1000, nullable = false)
 	private LocalDate startDate;
     
-    @NotNull(message = "{error.commons.required}")
-    @Column(length = 100, nullable = false)
+	@ManyToOne
+    @JoinColumn(nullable = false)
 	private FinancesType type;
     
 
